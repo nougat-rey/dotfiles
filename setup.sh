53584824dbdbc69eae332f6bc61005b2a50691bb
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-04-24 22:26:08 -0400

    Change Makefile into bash script to set up dotfiles

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cp -R $PWD/.vim $HOME/.vim
+
+if [ -f $HOME/.vimrc ]; then
+	rm $HOME/.vimrc
+fi
+ln -s $PWD/.vimrc $HOME/.vimrc
+
+if [ -f $HOME/.bashrc ]; then
+	rm $HOME/.bashrc
+fi
+ln -s $PWD/.bashrc $HOME/.bashrc
+
+if [ -f $HOME/.bash_profile ]; then
+	rm $HOME/.bash_profile
+fi
+ln -s $PWD/.bash_profile $HOME/.bash_profile
+
+if [ -f $HOME/.tmux.conf ]; then
+	rm $HOME/.tmux.conf
+fi
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+
